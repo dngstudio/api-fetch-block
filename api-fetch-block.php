a334a126797dd8226ef095b742d1e8d9206080e6
DNG Studio <jelena@optimized-marketing.com>
2023-06-26 21:18:26 +0200

    Fallback widget

--- api-fetch-block.php
+++ api-fetch-block.php
@@ -24,3 +24,45 @@ function create_block_api_fetch_block_block_init() {
 	register_block_type( __DIR__ . '/build' );
 }
 add_action( 'init', 'create_block_api_fetch_block_block_init' );
+
+class APIFetchWidget extends WP_Widget {
+    public function __construct() {
+        parent::__construct('api_fetch_widget', 'API Fetch Widget');
+    }
+
+    public function widget($args, $instance) {
+        echo $args['before_widget'];
+
+        // Fetch data from the API
+        $response = wp_remote_get('https://httpbin.org/get');
+        if (!is_wp_error($response) && $response['response']['code'] === 200) {
+            $data = json_decode(wp_remote_retrieve_body($response), true);
+            $headers = $data['headers'];
+
+            // Access and display the headers
+            if (!empty($headers)) {
+                echo '<ul>';
+                foreach ($headers as $name => $value) {
+                    echo '<li><strong>' . esc_html($name) . ':</strong> ' . esc_html($value) . '</li>';
+                }
+                echo '</ul>';
+            } else {
+                // Fallback content when headers are not available
+                echo 'No headers found in the API response.';
+            }
+        } else {
+            // Error handling or fallback content when API request fails
+            echo 'Error fetching data from the API.';
+        }
+
+        echo $args['after_widget'];
+    }
+
+}
+
+
+
+function register_api_fetch_widget() {
+    register_widget('APIFetchWidget');
+}
+add_action('widgets_init', 'register_api_fetch_widget');
