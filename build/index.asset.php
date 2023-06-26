c0f76487beb8109056ef23957c9580314caa3eb3
DNG Studio <jelena@optimized-marketing.com>
2023-06-26 22:54:25 +0200

    Build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '36fc654332f1d82e7c37');
+<?php return array('dependencies' => array('wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4a4967edddf3f174b5cc');
